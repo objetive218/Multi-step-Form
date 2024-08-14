d66266c673d041b3dde889b05237a1d459d6339c
objetive218 <davidsantiagotc@gmail.com>
2024-08-14 17:06:34 -0500

    add thankYou logic and style

--- src/components/ThankYou.jsx
+++ src/components/ThankYou.jsx
@@ -1,9 +1,9 @@
 // eslint-disable-next-line react/prop-types
 const ThankYou = () => {
     return (
-        <div>
+        <div className="thankYou">
             <img src="src\assets\images\icon-thank-you.svg" alt="thank you icon" />
-            <h2>Thank you!</h2>
+            <h1>Thank you!</h1>
             <p>Thanks for confirming your subscription! We hope you have fun using our platform. If you ever need support, please feel free to email us at support@lorempagming.com.</p>
         </div>
     );
