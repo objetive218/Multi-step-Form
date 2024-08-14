32e1480bbe9fd7d4569104445fb5ab8b4c76f13d
objetive218 <davidsantiagotc@gmail.com>
2024-08-14 17:06:18 -0500

    add step three logic and style

--- src/components/StepThree.jsx
+++ src/components/StepThree.jsx
@@ -1,34 +1,51 @@
+import { useState } from "react";
+
 // eslint-disable-next-line react/prop-types
-const StepThree = ({handler}) => {
+const StepThree = ({handler, setInfo, info}) => {
+    // eslint-disable-next-line react/prop-types
+    const durationBilling = info.duration;
+    const [addOns, setAddOns] = useState({online:false,storage:false, profile:false});
+    const getAddOnsForm = () => {       
+        if(durationBilling === "mo"){
+        const online = addOns.online ? 1: null;
+        const storage = addOns.storage ? 2: null;
+        const profile = addOns.profile ? 2: null;
+        setInfo({...info,onlineBilling:online,storageBilling:storage, profileBilling:profile  })
+        }else{
+        const online = addOns.online ? 10: null;
+        const storage = addOns.storage ? 20: null;
+        const profile = addOns.profile ? 20: null;
+        setInfo({...info,onlineBilling:online,storageBilling:storage, profileBilling:profile  })
+        } 
+    }
     return (
-        <section>
+        <section className="stepThree">
             <h1>Pick add-ons</h1>
             <p>Add-ons help enhance your gaming experience.</p>
-            <form action="#">
-            <div>
-                <input type="checkbox" name="" id="online" />
-                <h5>Online service</h5>
-                <p>Access to multiplayer games</p>
-                <p>{ `$1/${"duracion"}`}</p>
+            <form action="#" onSubmit={(e) => {
+                e.preventDefault();
+                handler('four');    
+                getAddOnsForm()
+            }}>
+            <div className={addOns.online? "box_addOns" : ""} >
+                <input  onClick={(e) => {setAddOns({...addOns, online:e.target.checked})}} type="checkbox" name="online" id="online" />
+                <h5><span>Online service</span> <br /> Access to multiplayer games</h5>
+                <p>{durationBilling === "mo" ? `$1/${durationBilling}` : `$10/${durationBilling}` }</p>
             </div>
-            <div>
-                <input type="checkbox" name="" id="storage" />
-                <h5>Larger storage</h5>
-                <p>Extra 1TB of cloud save</p>
-                <p>{ `$2/${"duracion"}`}</p>
+            <div className={addOns.storage? "box_addOns" : ""}>
+                <input onClick={(e) => {setAddOns({...addOns, storage:e.target.checked})}} type="checkbox" name="storage" id="storage" />
+                <h5><span>Larger storage</span> <br /> Extra 1TB of cloud save</h5>
+                <p>{durationBilling === "mo" ? `$2/${durationBilling}` : `$20/${durationBilling}`}</p>
             </div>
-            <div>
-                <input type="checkbox" name="" id="profile" />
-                <h5>Customizable Profile</h5>
-                <p>Custom theme on your profile</p>
-                <p>{ `$2/${"duracion"}`}</p>
+            <div className={addOns.profile? "box_addOns" : ""}>
+                <input onClick={(e) => {setAddOns({...addOns, profile:e.target.checked})}} type="checkbox" name="profile" id="profile" />
+                <h5><span>Customizable Profile</span> <br /> Custom theme on your profile</h5>
+                <p>{durationBilling === "mo" ? `$2/${durationBilling}` : `$20/${durationBilling}` }</p>
             </div>
-                <button onClick={(e) => {e.preventDefault();
+                <button className="btn_back" onClick={(e) => {e.preventDefault();
                     handler('two');
                 }} >Go Back</button>
-                <button onClick={(e) => {e.preventDefault();
-                    handler('four');
-                }} type="submit">Next step</button>
+                <button className="btn_next" type="submit">Next step</button>
                 </form>            
         </section>
     );
