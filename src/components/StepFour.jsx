898c8bbb5d0e4f1d7c3901d0e5826aaca256d04f
objetive218 <davidsantiagotc@gmail.com>
2024-08-14 17:06:06 -0500

    add step four logic and style

--- src/components/StepFour.jsx
+++ src/components/StepFour.jsx
@@ -1,34 +1,42 @@
+import { useState } from "react";
+
 // eslint-disable-next-line react/prop-types
-const StepFour = ({handler}) => {
+const StepFour = ({handler, info}) => {
+    const [allInfo , setAllInfo] = useState(info);
+    const billingDuration = allInfo.duration === "mo"? "Monthly": "Yearly";
+    const billingDurationNormal = allInfo.duration === "mo"? "Month": "Year";
+    const total = allInfo.plan + allInfo.onlineBilling+ allInfo.storageBilling + allInfo.profileBilling;
+    console.log(total)
     return (
-        <section>
+        <section className="stepFour">
             <h1>Finishing up</h1>
             <p>Double-check everything looks OK before confirming.</p>
             <form action="#">
             <div>
-                <h5>{`Arcade(${"duration"})`}</h5>
-                <a href="">Change</a>
-                <p>{ `$9/${"duracion"}`}</p>
+                <h5>{`${allInfo.planName}(${billingDuration})`} <br /> <a href="#"  onClick={() => handler("two")}>Change</a></h5>
+                <p>{ `$${allInfo.plan}/${allInfo.duration}`}</p>
             </div>
-            <div>
-                <h5>{`Online service(${"duration"})`}</h5>
-                <a href="">Change</a>
-                <p>{ `$1/${"duracion"}`}</p>
-            </div>
-            <div>
+
+            {allInfo.onlineBilling ? <div>
+                <h5>{`Online service`}</h5>
+                <p>{ `$${allInfo.onlineBilling}/${allInfo.duration}`}</p>
+            </div> : ""}
+            {allInfo.storageBilling?<div>
                 <h5>{`Larger storage(${"duration"})`}</h5>
-                <a href="">Change</a>
-                <p>{ `$2/${"duracion"}`}</p>
-            </div>
+                <p>{ `$${allInfo.storageBilling}/${allInfo.duration}`}</p>
+            </div>:""}
+            {allInfo.profileBilling?<div>
+                <h5>{`Customizable profile(${"duration"})`}</h5>
+                <p>{ `$${allInfo.profileBilling}/${allInfo.duration}`}</p>
+            </div>:""}
             <div>
-                <h5>{`Total (per${"duration"})`}</h5>
-                <a href="">Change</a>
-                <p>{ `$12/${"duracion"}`}</p>
+                <h5>{`Total (per ${billingDurationNormal})`}</h5>
+                <p>{ `$${total}/${allInfo.duration}`}</p>
             </div>
-                <button onClick={(e) => {e.preventDefault();
+                <button className="btn_back" onClick={(e) => {e.preventDefault();
                     handler('three');
                 }} >Go Back</button>
-                <button onClick={(e) => {e.preventDefault();
+                <button className="btn_finish" onClick={(e) => {e.preventDefault();
                     handler('Thank');
                 }} type="submit">Finish</button>
                 </form>            
